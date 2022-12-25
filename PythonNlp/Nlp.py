517004cda38ad8512cff2847b3dbaf1c80980ae4
gengyouchou <resisttempetation23@gmail.com>
2022-12-25 20:27:10 +0800

    fix: need using nltk.download database

--- PythonNlp/Nlp.py
+++ PythonNlp/Nlp.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import nltk
+nltk.download('punkt')
 
 def tokenize(text):
     tokens = nltk.word_tokenize(text)
+    print(tokens)
     return tokens
 
 tokenize("I love you")
