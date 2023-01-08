5d59eeefa4da68078a08de20949779a75704dd93
gengyouchou <resisttempetation23@gmail.com>
2023-01-08 21:29:35 +0800

    Test: nltk.corpus  names

--- PythonNlp/Nlp.py
+++ PythonNlp/Nlp.py
@@ -1,99 +1,30 @@
 #!/usr/bin/env python3
+#coding=utf-8
+import random
 import nltk
-nltk.download('punkt')
-nltk.download('propbank')
+nltk.download('names')
+from nltk.corpus import names
 
-# Load the propbank corpus
-corpus = nltk.corpus.propbank
+def gender_features(word): 
+    # '''''提取每个单词的最后一个字母作为特征'''  
+	return {'last_letter': word[-1]}
 
-def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    print(tokens)
-    return tokens
+def test():
+    # 先为原始数据打好标签  
+    labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])  
+    # 随机打乱打好标签的数据集的顺序，  
+    random.shuffle(labeled_names)  
+    # 从原始数据中提取特征（名字的最后一个字母， 参见gender_features的实现）  
+    featuresets = [(gender_features(name), gender) for (name, gender) in labeled_names]  
+    # 将特征集划分成训练集和测试集  
+    train_set, test_set = featuresets[500:], featuresets[:500]  
+    # 使用训练集训练模型（核心就是求出各种后验概率）  
+    classifier = nltk.NaiveBayesClassifier.train(train_set)  
+    # 通过测试集来估计分类器的准确性  
+    print(nltk.classify.accuracy(classifier, test_set))  
+    # 如果一个人的名字的最后一个字母是‘a’，那么这个人是男还是女  
+    print(classifier.classify({'last_letter': 'a'}))  
+    # 找出最能够区分分类的特征值  
+    classifier.show_most_informative_features(5)  
 
-def PropbankCorpusReaderPrint():
-    # Load the propbank corpus
-    corpus = nltk.corpus.propbank
-
-    # Access the list of files in the corpus
-    print(corpus.fileids())
-
-    # Access a specific file in the corpus
-    file = corpus.fileids()[0]
-    print(corpus.raw(file))
-
-    # Access the annotated propositions in the file
-    print(corpus.prop_sents(file))
-
-    # Access the part-of-speech tags for the words in the file
-    print(corpus.tagged_sents(file))
-
-
-# Define a function to extract features from the data
-def extract_features(sentence, index):
-    return {
-        'word': sentence[index],
-        'is_first': index == 0,
-        'is_last': index == len(sentence) - 1,
-        'is_capitalized': sentence[index][0].upper() == sentence[index][0],
-        'is_all_caps': sentence[index].upper() == sentence[index],
-        'is_all_lower': sentence[index].lower() == sentence[index],
-        'prefix-1': sentence[index][0],
-        'prefix-2': sentence[index][:2],
-        'prefix-3': sentence[index][:3],
-        'suffix-1': sentence[index][-1],
-        'suffix-2': sentence[index][-2:],
-        'suffix-3': sentence[index][-3:],
-        'prev_word': '' if index == 0 else sentence[index - 1],
-        'next_word': '' if index == len(sentence) - 1 else sentence[index + 1],
-        'has_hyphen': '-' in sentence[index],
-        'is_numeric': sentence[index].isdigit(),
-        'capitals_inside': sentence[index][1:].lower() != sentence[index][1:]
-    }
-
-# Define a function to extract the features for a whole sentence
-def sentence_features(sentence):
-    return [extract_features(sentence, index) for index in range(len(sentence))]
-
-# Define a function to extract the features for a whole proposition
-def prop_features(prop):
-    sentence = [word for word, pos in prop]
-    return [extract_features(sentence, index) for index in range(len(sentence))]
-
-# Define a function to extract the features for a whole sentence
-def sentence_features(sentence):
-    return [extract_features(word, index) for index, word in enumerate(sentence)]
-
-
-
-
-def PropbankCorpusReaderTrain():
-#    # Extract the features for all propositions in the corpus
-#     featuresets = [(prop_features(prop), pos) for prop in corpus.pos_sents() for word, pos in prop]
-
-#     # Split the data into train and test sets
-#     train_set, test_set = featuresets[100:], featuresets[:100]
-
-#     # Train a classifier
-#     classifier = nltk.NaiveBayesClassifier.train(train_set)
-
-#     # Test the classifier
-#     print(nltk.classify.accuracy(classifier, test_set))
-
-    # Extract the features for all words in the corpus
-    featuresets = [(extract_features(word, index), tag) for index, (word, tag) in enumerate(corpus.words())]
-
-    # Split the data into train and test sets
-    train_set, test_set = featuresets[100:], featuresets[:100]
-
-    # Train a classifier
-    classifier = nltk.NaiveBayesClassifier.train(train_set)
-
-    # Test the classifier
-    print(nltk.classify.accuracy(classifier, test_set))
-
-
-
-PropbankCorpusReaderTrain()
-
-
+test()
